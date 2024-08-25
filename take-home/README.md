2c501d1648c299d002e04d784e75cf5c9fbd920a
Michenka6 <amirkhon.alimov@gmail.com>
2024-08-25 10:28:33 +0200

    added validation

---
+++ take-home/README.md
@@ -0,0 +1,45 @@
+﻿# How to build
+```console
+    $ cd take-home
+    $ dotnet build
+```
+
+## Debug
+```console
+    $ dotnet run
+```
+In debug mode, 
+as far I know many optimizations are not applied, 
+so the results of timing solution heavily skewed towarsds in-built dictionary,
+as per the table below:
+
+| Solution        | Average Time~ (ms) |
+|-----------------|--------------------|
+| Dictionary      | 40                 |
+| Naive Trie      | 60                 |
+| Naive Trie Dict | 122                |
+| Array Trie      | 60                 |
+
+## Release
+```console
+    $ dotnet run -c Release
+```
+However, in release after many vm optimizations,
+there is significant improvements in these solutions.
+
+| Solution        | Average Time~ (ms) |
+|-----------------|--------------------|
+| Dictionary      | 40                 |
+| Naive Trie      | 42                 |
+| Naive Trie Dict | 100                |
+| Array Trie      | 21                 |
+
+# Validating
+The idea of validating the results is that every solution has to produce the same answer,
+so I would print them out into json and compare them.
+To run the validation, follow these steps:
+```console
+    $ cd take-home
+    $ dotnet run -c Release
+    $ python ./validation.py
+```
---
+++ take-home/validation.py
@@ -0,0 +1,46 @@
+﻿import json
+import sys
+
+def sort_json_file(input_file, output_file):
+    with open(input_file, 'r') as infile:
+        data = json.load(infile)
+
+    # Sort the JSON data
+    sorted_data = json.dumps(data, sort_keys=True, indent=4)
+
+    with open(output_file, 'w') as outfile:
+        outfile.write(sorted_data)
+
+def compare_json_files(file1, file2):
+    with open(file1, 'r') as f1, open(file2, 'r') as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    # Compare JSON data
+    if data1 == data2:
+        print(f"The JSON files are identical. {file1} {file2}" )
+    else:
+        print("The JSON files are different.")
+
+
+
+sort_json_file("./occurence-tables/01-solution.json", "./occurence-tables/01-solution-sorted.json")
+sort_json_file("./occurence-tables/02-solution.json", "./occurence-tables/02-solution-sorted.json")
+sort_json_file("./occurence-tables/03-solution.json", "./occurence-tables/03-solution-sorted.json")
+sort_json_file("./occurence-tables/04-solution.json", "./occurence-tables/04-solution-sorted.json")
+
+compare_json_files("./occurence-tables/01-solution-sorted.json","./occurence-tables/02-solution-sorted.json")
+compare_json_files("./occurence-tables/01-solution-sorted.json","./occurence-tables/03-solution-sorted.json")
+compare_json_files("./occurence-tables/01-solution-sorted.json","./occurence-tables/04-solution-sorted.json")
+
+compare_json_files("./occurence-tables/02-solution-sorted.json","./occurence-tables/01-solution-sorted.json")
+compare_json_files("./occurence-tables/02-solution-sorted.json","./occurence-tables/03-solution-sorted.json")
+compare_json_files("./occurence-tables/02-solution-sorted.json","./occurence-tables/04-solution-sorted.json")
+
+compare_json_files("./occurence-tables/03-solution-sorted.json","./occurence-tables/01-solution-sorted.json")
+compare_json_files("./occurence-tables/03-solution-sorted.json","./occurence-tables/02-solution-sorted.json")
+compare_json_files("./occurence-tables/03-solution-sorted.json","./occurence-tables/04-solution-sorted.json")
+
+compare_json_files("./occurence-tables/04-solution-sorted.json","./occurence-tables/01-solution-sorted.json")
+compare_json_files("./occurence-tables/04-solution-sorted.json","./occurence-tables/02-solution-sorted.json")
+compare_json_files("./occurence-tables/04-solution-sorted.json","./occurence-tables/03-solution-sorted.json")
